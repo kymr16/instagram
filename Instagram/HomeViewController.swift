bb9745b31217a5659d6137d6e8f8ee754f2bc974
kymr16 <kym.m1116@gmail.com>
2020-07-05 22:29:19 +0900

    環境用意

---
+++ Instagram/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by 吉田 玲子 on 2020/07/04.
+//  Copyright © 2020 reiko.yoshida. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram/ImageSelectViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ImageSelectViewController.swift
+//  Instagram
+//
+//  Created by 吉田 玲子 on 2020/07/04.
+//  Copyright © 2020 reiko.yoshida. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
